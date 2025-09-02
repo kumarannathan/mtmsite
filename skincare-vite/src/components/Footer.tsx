c5465400b1dc83b0ffd299d63b2650a0c846d415
Kumaran Nathan <kumarann@umich.edu>
2025-09-01 22:53:50 -0400

    Update Footer component to include a new contact number and enhance styling for better visibility. Modify translation files to reflect changes in service titles and descriptions, emphasizing health and relaxation themes for improved clarity in Spanish.

--- skincare-vite/src/components/Footer.tsx
+++ skincare-vite/src/components/Footer.tsx
@@ -144,6 +144,19 @@ export default function Footer() {
               </svg>
               mtmreserv@gmail.com
             </a>
+            <a  style={{
+                color: '#1B4D3E',
+                textDecoration: 'none',
+                fontSize: '16px',
+                lineHeight: '1.8',
+                display: 'flex',
+                alignItems: 'center',
+                gap: '8px',
+                transition: 'color 0.2s ease'
+              }}> 
+              +56 12033702
+              
+            </a>
             <a 
               href="https://www.google.com/maps/place/MTM+Oriental+Capillary+Treatments/@22.1380798,-101.0160815,856m/data=!3m1!1e3!4m15!1m8!3m7!1s0x842a98da9ff1f979:0x7b611dd395a16dcc!2sJoaqu%C3%ADn+Meade+136,+Las+Lomas+1ra+Secc,+78290+San+Luis+Potos%C3%AD,+S.L.P.,+Mexico!3b1!8m2!3d22.1380798!4d-101.0160815!16s%2Fg%2F11c5ks7lf8!3m5!1s0x842a996f6d8cddc5:0x8a4ae976b3f47fba!8m2!3d22.1380798!4d-101.0160815!16s%2Fg%2F11xvc0xj7q?entry=ttu&g_ep=EgoyMDI1MDgyNS4wIKXMDSoASAFQAw%3D%3D" 
               target="_blank"
